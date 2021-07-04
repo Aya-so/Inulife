150d872c27abac67d063e6b98e6863fd05bad264
Aya-so <ayaka.wakame0315@gmail.com>
2021-07-04 03:18:53 +0000

    [Fix]devise修正

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_07_03_123557) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
